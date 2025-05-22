b2dea1cd41932315ef7b40d24e49cc65a25674d5
Daniel Silva <88464213+DanielKatsuo@users.noreply.github.com>
2025-05-21 23:15:43 -0300

    refactor: Reorganização do projeto e renomeação de classes
    Movido o `HomeController` para `ThermoWave.Web.Controllers` como `HomeController1`.
    O `ErrorViewModel` e o `TimerModel` foram transferidos para `ThermoWave.Web.Models`.
    Duplicado e renomeado o arquivo de projeto para `ThermoWave.Web.csproj`.
    Vários arquivos de visualização foram modificados para remover conteúdo desnecessário,
    incluindo títulos e seções de erro. O layout principal foi simplificado,
    removendo seções de navegação e scripts. Atualizada a solução para incluir o novo projeto `ThermoWave.Web`.

--- ThermoWave/Models/TimerModel.cs
+++ ThermoWave/Models/TimerModel.cs
@@ -1,8 +1,7 @@
-﻿namespace ThermoWave.Models
+﻿namespace ThermoWave.Web.Models
 {
 	public class TimerModel
 	{
-		public TimeOnly TempoAtual { get; set; } = TimeOnly.FromDateTime(DateTime.MinValue);
-		public int PotenciaAtual { get; set; } = 10;
+
 	}
 }
